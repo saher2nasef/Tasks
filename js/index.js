57549765f548861faacfbe50f8feafb6e8e9e38a
saher_nasef <81627217+saher2nasef@users.noreply.github.com>
2022-12-20 14:58:11 -0800

    Add files via upload

--- js/index.js
+++ js/index.js
@@ -1,40 +1,85 @@
 let checkBox = document.getElementById("checkBox");
 let TextToDo = document.getElementById("TextToDo");
 let InnerData = document.getElementById("InnerData");
+let ClearCompleted = document.getElementById("ClearCompleted");
 let Btn = document.getElementById("Btn");
+let Info = document.getElementById("Info");
+let btns = document.getElementById("btns").children;
 let body = document.querySelector("body");
 // To toggle the theme
 let Bool = false;
+var r = document.querySelector(":root");
 Btn.addEventListener("click", () => {
-  var r = document.querySelector(":root");
-
   if (Bool == false) {
     Bool = true;
-    Btn.src = "images/icon-sun.svg";
-    r.style.setProperty("--Color", "#fff");
-    r.style.setProperty("--Bg", "hsl(235, 24%, 19%)");
-    r.style.setProperty("--Bg1", "hsl(235, 21%, 11%)");
-    body.classList.toggle("active");
-    // Moon
-  } else {
-    Bool = false;
+
     Btn.src = "images/icon-moon.svg";
     r.style.setProperty("--Color", "hsl(235, 19%, 35%)");
     r.style.setProperty("--Bg", " #fff");
     r.style.setProperty("--Bg1", "#fff");
     body.classList.toggle("active");
     // Sun
+  } else {
+    Bool = false;
+    Btn.src = "images/icon-sun.svg";
+    r.style.setProperty("--Color", "#fff");
+    r.style.setProperty("--Bg", "hsl(235, 24%, 19%)");
+    r.style.setProperty("--Bg1", "hsl(235, 21%, 11%)");
+    body.classList.toggle("active");
+    // Moon
   }
 });
+// For Resort
+for (let i = 0; i < btns.length; i++) {
+  btns[i].addEventListener("click", () => {
+    for (let x = 0; x < btns.length; x++) {
+      btns[x].classList.remove("active");
+    }
+    btns[i].classList.add("active");
+    if (btns[i].dataset.type === "All") {
+      let Data = JSON.parse(localStorage.getItem("Data"));
+      FullDiv(Data);
+      ChangeData();
+    } else if (btns[i].dataset.type === "Active") {
+      let Data = JSON.parse(localStorage.getItem("Data"));
+      let Result = Data.filter((Item) => Item.Active == false);
+      FullDiv(Result);
+      ChangeData((have = true), (length = Result.length));
+    } else if (btns[i].dataset.type === "Completed") {
+      let Data = JSON.parse(localStorage.getItem("Data"));
+      let Result = Data.filter((Item) => Item.Active == true);
+      FullDiv(Result);
+      ChangeData((have = true), (length = Result.length));
+    }
+  });
+}
+// ClearCompleted
+ClearCompleted.addEventListener("click", () => {
+  let Data = JSON.parse(localStorage.getItem("Data"));
+  let Result = Data.filter((Item) => Item.Active == true);
+  for (let i = 0; i < Result.length; i++) {
+    Data.splice(Data.indexOf(Result[i]), 1);
+  }
+  localStorage.setItem("Data", JSON.stringify(Data));
+  FullDiv(Data);
+  ChangeData();
+});
+// Load
 window.onload = () => {
   if (localStorage.getItem("Data") === null) {
     localStorage.setItem("Data", JSON.stringify([]));
   } else {
     let Data = JSON.parse(localStorage.getItem("Data"));
-
     FullDiv(Data);
+    ChangeData();
   }
+  Btn.src = "images/icon-sun.svg";
+  r.style.setProperty("--Color", "#fff");
+  r.style.setProperty("--Bg", "hsl(235, 24%, 19%)");
+  r.style.setProperty("--Bg1", "hsl(235, 21%, 11%)");
+  body.classList.toggle("active");
 };
+// On write add Task
 TextToDo.addEventListener("keydown", (event) => {
   if (
     event.key === "Enter" &&
@@ -52,6 +97,7 @@ TextToDo.addEventListener("keydown", (event) => {
     FullDiv(Data);
     TextToDo.value = "";
     checkBox.checked = false;
+    ChangeData();
   }
 });
 function DeleteById(Id) {
@@ -60,15 +106,23 @@ function DeleteById(Id) {
   Data.splice(Data.indexOf(Result[0]), 1);
   localStorage.setItem("Data", JSON.stringify(Data));
   FullDiv(Data);
+  for (let x = 0; x < btns.length; x++) {
+    btns[x].classList.remove("active");
+  }
+  btns[0].classList.add("active");
+  ChangeData();
 }
 function EditById(Id) {
   let Data = JSON.parse(localStorage.getItem("Data"));
   let Result = Data.filter((Item) => Item.Id == Id);
   let Value = Data[Data.indexOf(Result[0])].Active;
   Data[Data.indexOf(Result[0])].Active = !Value;
-  console.log(Data);
   localStorage.setItem("Data", JSON.stringify(Data));
   FullDiv(Data);
+  for (let x = 0; x < btns.length; x++) {
+    btns[x].classList.remove("active");
+  }
+  btns[0].classList.add("active");
 }
 function FullDiv(Data) {
   InnerData.innerHTML = "";
@@ -89,5 +143,16 @@ function FullDiv(Data) {
       </button>
     </div>
     `;
+  }
+}
+function ChangeData(have = false, length = 0) {
+  let Data = JSON.parse(localStorage.getItem("Data"));
+  console.log(Data.length);
+  if (have == true) {
+    Info.innerHTML = `${length} items left`;
+  } else {
+    Info.innerHTML = `${
+      Data.length > 1 ? Data.length + " items left" : "1 items left"
+    }`;
   }
 }
